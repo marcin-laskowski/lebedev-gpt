295bfd6eadb571aed3ed4753c8eb4cee75cdeb13
SatyukovED <satukov-egor@mail.ru>
2021-10-11 19:14:41 +0300

    Init project

---
+++ syndicai.py
@@ -0,0 +1,22 @@
+from source.generate_transformers import main, parse_args, get_model
+
+class PythonPredictor:
+    def __init__(self, config):
+        parser = parse_args()
+        args = parser.parse_args(['--model_type', 'gpt2', 
+                                  '--model_name_or_path', './model',
+                                  '--k', '50',
+                                  '--p', '0.95',
+                                  '--length', '100',
+                                  '--repetition_penalty', '5',
+                                  '--num_return_sequences', '1',
+                                  '--no_cuda'])
+        model, tokenizer = get_model(args)
+        self.args = args
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def predict(self, payload):
+        self.args.prompt = payload['text']
+        output_text = self.model.main(model, tokenizer, args)
+        return output_text
