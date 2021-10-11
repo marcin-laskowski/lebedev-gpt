91b3264211ff172f40f2fca600121450aeb35bc4
SatyukovED <satukov-egor@mail.ru>
2021-10-11 20:03:30 +0300

    Change reqs and syndicai.py

--- syndicai.py
+++ syndicai.py
@@ -1,22 +1,28 @@
-from source.generate_transformers import main, parse_args, get_model
+# from source.generate_transformers import main, parse_args, get_model
 
 class PythonPredictor:
-    def __init__(self, config):
-        parser = parse_args()
-        args = parser.parse_args(['--model_type', 'gpt2', 
-                                  '--model_name_or_path', './model',
-                                  '--k', '50',
-                                  '--p', '0.95',
-                                  '--length', '100',
-                                  '--repetition_penalty', '5',
-                                  '--num_return_sequences', '1',
-                                  '--no_cuda'])
-        model, tokenizer = get_model(args)
-        self.args = args
-        self.model = model
-        self.tokenizer = tokenizer
+    # def __init__(self, config):
+    #     parser = parse_args()
+    #     args = parser.parse_args(['--model_type', 'gpt2', 
+    #                               '--model_name_or_path', './model',
+    #                               '--k', '50',
+    #                               '--p', '0.95',
+    #                               '--length', '100',
+    #                               '--repetition_penalty', '5',
+    #                               '--num_return_sequences', '1',
+    #                               '--no_cuda'])
+    #     model, tokenizer = get_model(args)
+    #     self.args = args
+    #     self.model = model
+    #     self.tokenizer = tokenizer
 
-    def predict(self, payload):
-        self.args.prompt = payload['text']
-        output_text = main(self.model, self.tokenizer, self.args)
-        return output_text
+    # def predict(self, payload):
+    #     self.args.prompt = payload['text']
+    #     output_text = main(self.model, self.tokenizer, self.args)
+    #     return output_text
+
+class PythonPredictor:    
+    def __init__(self, config):        
+        pass    
+    def predict(self, payload):        
+        return payload["key"]  # prints "value"
